e331f89b8e391f716bc121e979d219ba3328487f
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-10-07 01:49:56 +0530

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./componants/Header/Header"
+import {Header,Navbar}  from "./componants"
+import Secondlayout from './componants/Secondlayout/Secondlayout'
+import Corosal from './componants/Corosal/Corosal'
+import InvestmentCalculator from './componants/investmentCalculator/InvestmentCalculator'
+import Bargraphs from './componants/bargraps/Bargraphs'
+import Testimonials from './componants/testimonials/Testimonials'
+import Frequent from './componants/Frequentquestions/Frequent'
+import Footer from './componants/footer/Footer'
+import Douhnut from './componants/investmentCalculator/Douhnut'
+const App = () => (
+  <div className='app'>
+    <Navbar/>
+    <Header />
+    <Secondlayout />
+    <Corosal />
+    <Bargraphs />
+    <InvestmentCalculator />
+    <Testimonials /> 
+    <Frequent/>
+    <Footer/>
+  </div>
+
+)
+
+export default App
